c9dfd1707b9443186bb84689d342bc1b7aff33d0
Aurélie Constant <aurelie.constant@outlook.com>
2020-09-16 12:04:18 +0200

    no message

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -40,7 +43,7 @@ app.get('/rayons/:nom_rayon',function(req,res){
     let name = req.params.nom_rayon;
 //variable de tri pour mettre en premier les produits dont le champ mis_en_avant est à "oui" (après "non" dans l'ordre alphabétique).
     let mysort = {mis_en_avant:-1};
-    Produit.find({categorie:name},function(err,rayon){
+    Produit.find({publication:"oui",categorie:name},function(err,rayon){
         console.log(name);
         console.log(rayon);
         res.render('rayons',{rayon:rayon})
@@ -51,10 +54,15 @@ app.get('/rayons/:nom_rayon',function(req,res){
 app.get('/tous_nos_produits',function(req,res){
 //tri comme pour le find précédent
   let mysort = {mis_en_avant:-1};
-  Produit.find({},function(err,selection){
+  Produit.find({publication:"oui"},function(err,selection){
     console.log(selection);
     res.render('tous_nos_produits',{selection:selection});
   }).sort(mysort)
+})
+
+//affichage page admin
+app.get('/admin',function(req,res){
+    res.render('admin');
 })
 
 //afficher autres pages
@@ -81,6 +89,47 @@ app.get('/mentions-legales',function(req,res){
 app.get('/contact',function(req,res){
     res.render('contact');
 })
+
+//gestion du formulaire d'ajout de produits
+app.post('/ajout',function(req,res){
+  let publie = req.body.ajout_publication;
+  if (publie == "on") {
+    publie = "oui";
+  }
+  else {
+    publie = "non";
+  };
+
+  let avant = req.body.ajout_avant;
+  if (avant == "on") {
+    avant = "oui";
+  }
+  else {
+    avant = "non";
+  };
+
+  let cat = req.body.ajout_categorie;
+
+  let nouveau = new Produit ({nom_produit:req.body.ajout_nom});
+  nouveau.nom_producteur = req.body.ajout_producteur;
+  nouveau.prix = req.body.ajout_prix;
+  nouveau.categorie = cat;
+  nouveau.description = req.body.ajout_description;
+  nouveau.reference = req.body.ajout_reference;
+  nouveau.stock = req.body.ajout_stock;
+  nouveau.unite = req.body.ajout_unite;
+  nouveau.TVA = req.body.ajout_tva;
+  nouveau.image = req.body.ajout_image;
+  nouveau.publication = publie;
+  nouveau.mis_en_avant = avant;
+  //sauvegarder dans la bdd
+  nouveau.save();
+  console.log(nouveau);
+  res.redirect('/rayons/'+ cat)
+})
+
+
+
 
 //Gérer erreurs 404
 app.use(function(req,res){
